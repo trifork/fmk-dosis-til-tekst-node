f82ceea44dbf2d2c88411feb1dd0aad222f3729a
ahpatrifork <ahpa@trifork.com>
2022-11-23 14:21:17 +0100

    log every request

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,25 @@ import express, {Application} from 'express';
 import swaggerUi from 'swagger-ui-express';
 
 import Router from './routes';
+import {logger} from './logger';
 
 const PORT = process.env.PORT || 8000;
 
+// express.js
 const app: Application = express();
-
 app.use(express.json());
+
+// logging every request with pino
+const myLogger = function (req: any, res: any, next: () => void) {
+    logger.info('Request received: ' + req.path);
+    next();
+};
+app.use(myLogger);
+
+// routing
 app.use(Router);
 
+// swagger
 const swaggerDocument = require('./swagger.json');
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
