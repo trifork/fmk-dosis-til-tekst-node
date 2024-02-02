4f409ad0e37ff82436e738fd32e17af2b949eca0
Flemming Jørgensen <fjo@trifork.com>
2024-02-02 06:32:00 +0100

    JTE conversion

---
+++ pipeline_config.groovy
@@ -0,0 +1,8 @@
+libraries{
+    node
+    docker {
+        image_name = 'registry.fmk.netic.dk/fmk/fmk-dosistiltekst-server'
+        image_user = 'fmk-dosistiltekst'
+        java_version = 11
+    }
+}
