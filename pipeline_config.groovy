10b0ce2cc0044535bb2469c97ffb23298f939bad
Flemming Jørgensen <fjo@trifork.com>
2024-06-24 14:58:18 +0200

    Fix name

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -4,7 +4,7 @@ libraries{
     }
     maven
     docker {
-        output_images = '[{"name":"fmk-dosistiltekst","image":"registry.fmk.netic.dk/fmk/fmk-dosistiltekst-server","user":"fmk-dosistiltekst"}]'
+        output_images = '[{"name":"dosis2text","image":"registry.fmk.netic.dk/fmk/fmk-dosistiltekst-server","user":"fmk-dosistiltekst"}]'
     }
     sonarqube_maven
 }
