6399ed22618fbfc41e3b21db6c47b5fcb07ecd0c
Flemming Jørgensen <fjo@trifork.com>
2024-06-24 12:51:31 +0200

    Try with maven

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -1,9 +1,7 @@
 libraries{
-    node
+    maven
     docker {
-        image_name = 'registry.fmk.netic.dk/fmk/fmk-dosistiltekst-server'
-        image_user = 'fmk-dosistiltekst'
-        java_version = 11
+        output_images = '[{"name":"fmk-dosistiltekst","image":"registry.fmk.netic.dk/fmk/fmk-dosistiltekst-server","user":"fmk-dosistiltekst"}]'
     }
-    sonarqube_node
+    sonarqube_maven
 }
