53dfd71a6de845a9e6c97dbb1e2464d2fc8b22dc
Jeppe Sommer <jso@trifork.com>
2025-06-18 14:09:26 +0200

    FMK-10789 Moderniseret fmk-dosis-til-tekst-node baseret på fmk-dosis-til-tekst-ts v/2

--- src/logger/logger.ts
+++ src/logger/logger.ts
@@ -1,12 +1,28 @@
+// import pino from 'pino';
+// import { getRequestContext } from './requestContext';
+
+// export const logger = pino({
+//     transport: {
+//         target: './pino-pretty-transport',
+//         options: {
+//             translateTime: 'SYS:yyyy-mm-dd HH:MM:ss,l'
+//         }
+//     },
+//     level: 'info'
+// });
+
+
 import pino from 'pino';
+import { createPrettyStream } from './pinoPrettyTransport';
+import { getRequestContext } from './requestContext'; // Optional: for requestId support
 
-
-export const logger = pino({
-    transport: {
-        target: './pino-pretty-transport',
-        options: {
-            translateTime: 'SYS:yyyy-mm-dd HH:MM:ss,l'
-        }
+export const logger = pino(
+    {
+        mixin() {
+            const context = getRequestContext();
+            return { requestId: context?.requestId || '-' };
+        },
     },
-    level: 'info'
-});
+    createPrettyStream()
+);
+
--- src/logger/pino-pretty-transport.ts
+++
@@ -1,6 +0,0 @@
-module.exports = (opts: any) => require('pino-pretty')({
-    ...opts,
-    customPrettifiers: {
-        time: (timestamp: any) => timestamp
-    }
-});
