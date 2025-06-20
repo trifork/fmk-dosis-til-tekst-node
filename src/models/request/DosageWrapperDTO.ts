8c72be3046777af72267cacbb3da06a94f9b7211
Jeppe Sommer <jso@trifork.com>
2025-06-20 12:47:52 +0200

    FMK-10905 Angiv dosering-start og -slut med DateOnly i stedet for DataOrDateTime

--- src/models/request/DosageWrapperDTO.ts
+++ src/models/request/DosageWrapperDTO.ts
@@ -1,4 +1,6 @@
-import { Dosage } from "fmk-dosis-til-tekst-ts";
+// TODO Use import on next line and remove LegacyDosage.ts when all clients are updated
+// import { Dosage } from "fmk-dosis-til-tekst-ts";
+import { Dosage } from "./LegacyDosage";
 
 export interface DosageWrapperDTO {
   /** @deprecated */ dosageJson?: string;
